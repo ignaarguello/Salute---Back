bd9d53d3d5838ef12b5dbaaec5fdc30019f9ed1c
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-02-27 14:26:30 -0300

    modelos de prueba de productos

--- models/data/productos.js
+++ models/data/productos.js
@@ -3,21 +3,24 @@ let productos = [
         nombre: 'Fernet Branca 750ml',
         tipo: 'Aperitivo',
         precio: 1900,
-        imagen: ''
+        imagen: 'http://d2r9epyceweg5n.cloudfront.net/stores/001/769/691/products/fernet-branca-1lt_1lt_11-20b4d38d3cead1b0f716267030672745-640-0.jpg'
     },
     {
         nombre: 'Andes Roja 473cc (lata)',
         tipo: 'Cerveza',
         precio: 500,
-        imagen: ''
+        imagen: 'https://s1874.com/as/wp-content/uploads/2020/03/04-cerveza-andes-roja-lata-x-473.jpg'
     },
     {
         nombre: 'Cosecha especial Extra Brut de Norton',
         tipo: 'Espumante',
         precio: 3200,
-        imagen: ''
+        imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3IED7IA5WU_xqrMhhyM5x2mqBfJh5Uy60Qv-mszWH9sl-MwCI-lkmy7mavJCHcBz8yVg&usqp=CAU'
     },
 ]
+
+require('dotenv').config()
+require('../../config/database')
 
 const Producto = require('../Producto')
 
