88859e7fd36e35a483519da33ab25aa38a7e85b3
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-03-23 17:28:26 -0300

    ruteo para zonas de entregas

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 let router = require('express').Router()
 let producto = require('./producto')
 let usuario = require('./usuarios')
+let zonaEntrega = require('./zonas')
 
 router.use('/productos', producto)
 router.use('/usuarios', usuario)
+router.use('/zonas', zonaEntrega)
 
 module.exports = router;
