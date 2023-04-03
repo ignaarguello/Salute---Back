c1924a4e487283d6d99a38b7f0592eaefa132fa8
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-04-03 13:24:33 -0300

    propiedad usuarioID se quita el required: true

--- models/Producto.js
+++ models/Producto.js
@@ -6,7 +6,7 @@ const schema = new mongoose.Schema({
     precio: {type: Number, required: true},
     imagen: {type: String, required: true},
     enCarrito: {type: Boolean, default: false},
-    usuarioId: {type: mongoose.Types.ObjectId, ref: 'usuarios', required: true}
+    usuarioId: {type: mongoose.Types.ObjectId, ref: 'usuarios'}
 })
 
 const Producto = mongoose.model('productos', schema)
