b6c3093493061e6e81bf28096f4a76ff17226e12
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-04-03 13:24:50 -0300

    elimina modelos y controladores no utilizados

--- models/Compra.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const schema = new mongoose.Schema({
-    usuario: {type: mongoose.Types.ObjectId, ref: 'usuarios', required: true},
-    productos: [{type: mongoose.Types.ObjectId, ref: 'productos', required: true}],
-})
-
-const Compra = mongoose.model('compras', schema)
-
-module.exports = Compra
